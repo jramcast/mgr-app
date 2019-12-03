ea2deddb60f43c58d6d33f35310ea6853996d2a0
Jaime Ramírez <jaime.ramirez@roiback.com>
2019-12-03 09:00:52 +0100

    Updated api endpoint

--- source/index.ts
+++ source/index.ts
@@ -17,7 +17,7 @@ const form = new SongForm(songFormElementId, songInputElementId);
 const player = new YouTubePlayer(playerElementId);
 
 // Setup backend api adapter
-const apiBaseUrl = "http://9f1a5f0e-03f3-4dd4-b222-a314c9eea74d.pub.cloud.scaleway.com:5000"
+const apiBaseUrl = "https://9f1a5f0e-03f3-4dd4-b222-a314c9eea74d.pub.cloud.scaleway.com"
 const backendAPI = new BackendAPIAxiosAdapter(apiBaseUrl);
 
 // Setup results presenter
